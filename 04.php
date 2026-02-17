e325a0d955a7a96fd1b6b9e1943c5c6b190134dc
Mauricio <maau.cursos@gmail.com>
2026-02-17 12:49:41 -0600

    Herencias de clases

--- 04.php
+++ 04.php
@@ -1,4 +1,59 @@
 <?php include 'includes/header.php';
 
+// Herencia 
+
+class Transporte {
+    
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+
+    }
+
+    public function getInfo() : string
+    {
+        return "El transporte tiene " . $this->ruedas . "ruedas y una capacidad de " . $this->capacidad . " personas "; 
+   
+        }
+
+    public function getRuedas() : int {
+        return $this->ruedas; 
+    }
+}
+
+
+class Bicitleta extends Transporte {
+
+    public function getInfo(): string
+    {
+         return "El transporte tiene " . $this->ruedas . "ruedas y una capacidad de " . $this->capacidad . " personas y NO GASTA COMBUSTIBLE "; 
+
+        
+    }
+
+}
+
+class Automovil extends Transporte {
+     public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision)
+    {
+
+    }
+
+    public function getTransmision() : string{
+        return $this->transmision;
+    }
+
+}
+$bicicleta = new Bicitleta(2,1); 
+echo $bicicleta->getInfo(); 
+echo $bicicleta->getRuedas();
+
+
+echo '<hr>';   
+
+$auto = new Automovil(4,4, 'manual');
+echo $auto->getInfo(); 
+echo $auto->getTransmision();
+
+
 
 include 'includes/footer.php';
