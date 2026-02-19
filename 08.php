7a73f272da101f3c43830abb3fcf768625efc85a
Mauricio <maau.cursos@gmail.com>
2026-02-19 17:45:25 -0600

    Autoload de clases

--- 08.php
+++ 08.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php';
 
+// Incluir las otras clases 
+
+// require 'clases/clientes.php';
+// require 'clases/detalles.php';
+
+function mi_autoload($clase){
+    echo $clase;
+    require __DIR__ . '/clases/' . $clase . '.php';
+    
+}
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+$clientes = new Clientes(); 
 
 include 'includes/footer.php';
---
+++ clases/detalles.php
@@ -0,0 +1,8 @@
+<?php
+
+class Detalles {
+    public function __construct()
+    {
+        echo "Detalles de la clase Detalles.php";    
+    }
+}
