92ac5225c4b058e07d48a22a55290be8d45b17ef
Mauricio <maau.cursos@gmail.com>
2026-02-19 16:51:58 -0600

    Interfaces

--- 05.php
+++ 05.php
@@ -1,6 +1,6 @@
 <?php include 'includes/header.php';
 
-// Herencia 
+// Clase abstracta 
 
 abstract class Transporte {
     
--- 06.php
+++ 06.php
@@ -1,4 +1,31 @@
 <?php include 'includes/header.php';
 
+// Interfaz para definir el contrato de transporte
+
+interface TransporteInterfaz {
+
+    public function getInfo() : string; 
+    public function getRuedas() : int; 
+   
+}
+
+class Transporte implements TransporteInterfaz {
+    
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+
+    }
+
+    public function getInfo() : string
+    {
+        return "El transporte tiene " . $this->ruedas . "ruedas y una capacidad de " . $this->capacidad . " personas "; 
+   
+        }
+
+    public function getRuedas() : int {
+        return $this->ruedas; 
+    }
+}
+
 
 include 'includes/footer.php';
