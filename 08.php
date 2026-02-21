7778b5fea4aea96a2bfd1c2013814816c6721a7f
mauaguirre1235 <maau.cursos@gmail.com>
2026-02-20 23:02:35 -0600

    Autoload con composer

--- 08.php
+++ 08.php
@@ -1,30 +1,18 @@
 <?php include 'includes/header.php';
 
-// // Incluir las otras clases 
 
-
+require 'vendor/autoload.php'; 
 // require 'clases/Clientes.php';  
-
 // require 'clases/Detalles.php';  
 
 use App\Clientes;
 use App\Detalles;
-
-function mi_autoload($clase){
-  
-    $parte = explode('\\', $clase); 
-     
-
-  
-     require __DIR__ . '/clases/' . $parte[1] . '.php';
-    
-}
-spl_autoload_register('mi_autoload');
+use Firebase\JWT\JWT;
 
 
 
-$detalles = new App\Detalles();
-$clientes = new App\Clientes(); 
-$clientes2 = new Clientes();
+$detalles = new Detalles();
+$clientes = new Clientes(); 
+
 
 include 'includes/footer.php';
